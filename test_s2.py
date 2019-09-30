21096dd428c07ae0b430587d99aeb1759f20c50a
Antoine HEITZMANN <heitzmaa@iut-acy.local>
2019-09-30 09:35:30 +0200

    testing average function

--- test_s2.py
+++ test_s2.py
@@ -5,15 +5,22 @@ Created on Sun Sep 29 21:44:54 2019
 
 @author: alben
 """
-
+import S1_algotools as algo
 import pytest
 
+"""
 def inc_(x):
     return x+1
 
 def test_inc():
-    assert inc_(3)==5
+    assert inc_(3)==4
 
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
         1/0
+"""
+
+def test_average_above_zero_V1():
+   tableau=[0,3,5]
+   test, lastID=algo.average_above_zero(tableau)
+   assert test ==4
